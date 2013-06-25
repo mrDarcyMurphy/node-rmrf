cac64b956a032f33d952a3e4fec2d2e41e14e03d
Darcy Murphy <mrDarcyMurphy@gmail.com>
2013-06-25 11:09:54 -0500

    rmrf mf

---
+++ index.js
@@ -0,0 +1,14 @@
+var fs = require('fs')
+
+module.exports = function(dirPath) {
+  var files = fs.readdirSync(dirPath)
+  if (files.length > 0)
+    for (var i = 0; i < files.length; i++) {
+      var filePath = dirPath + '/' + files[i]
+      if (fs.statSync(filePath).isFile())
+        fs.unlinkSync(filePath)
+      else
+        rmdir(filePath)
+    }
+  fs.rmdirSync(dirPath)
+}
