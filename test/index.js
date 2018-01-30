33e370ba434471d3443d1e356f4ff1d9045a1d0b
Mr. Darcy Murphy <darcy.murphy@loudbit.com>
2018-01-29 18:51:10 -0600

    Test symlink directory behavior

--- test/index.js
+++ test/index.js
@@ -1,32 +1,56 @@
 var rmrf = require('../index.js')
 var fs = require('fs')
 var assert = require('assert')
-var existingPath = __dirname + '/exists'
 
-// not bothering with 'before' as it was unreliable
-if (!fs.existsSync(__dirname + '/exists')) {
-  fs.mkdirSync(__dirname + '/exists')
-  fs.mkdirSync(__dirname + '/exists/nested')
-  fs.openSync( __dirname + '/exists/nested/empty.txt', 'w')
-}
+describe('rmrf', function() {
 
-describe('rmrf', function(){
-  describe('when target is missing', function(){
-    it('does not throw', function(){
-      assert.doesNotThrow(function(){
+  describe('when target is missing', function() {
+    it('does not throw', function() {
+      assert.doesNotThrow(function() {
         rmrf(__dirname + '/missing')
       })
     })
   })
-  describe('when target exists', function(){
-    it('does not throw', function(){
-      assert.doesNotThrow(function(){
+
+  describe('when target exists', function() {
+    before(function() {
+      fs.mkdirSync(__dirname + '/exists')
+      fs.mkdirSync(__dirname + '/exists/nested')
+      fs.openSync( __dirname + '/exists/nested/empty.txt', 'w')
+    })
+
+    it('does not throw', function() {
+      assert.doesNotThrow(function() {
         rmrf(__dirname + '/exists')
       })
     })
-    it('removes the folders', function(){
-      assert.throws(function(){
+
+    it('removes the folders', function() {
+      assert.throws(function() {
         fs.readdirSync(__dirname + '/exists')
+      })
+    })
+  })
+
+  describe('when target is symlinked', function() {
+    before(function() {
+      fs.mkdirSync(__dirname + '/symlinked')
+      fs.openSync( __dirname + '/symlinked/important_file.txt', 'w')
+      fs.mkdirSync(__dirname + '/symlink_container')
+      fs.symlinkSync(__dirname + '/symlinked', __dirname + '/symlink_container/symlinked')
+    })
+
+    after(function() {
+      rmrf(__dirname + '/symlinked')
+    })
+
+    it('should not remove symlinked folder', function() {
+      rmrf(__dirname + '/symlink_container')
+      assert.doesNotThrow(function() {
+        fs.readdirSync(__dirname + '/symlinked');
+      })
+      assert.throws(function() {
+        fs.readdirSync(__dirname + '/symlink_container');
       })
     })
   })
